98a5e297be710539d88093ef4afccb79b854cc82
pedromlionco11-dotcom <pedromlionco11@gmail.com>
2026-09-23 11:05:39 -0300

    bugfix: corrigindo bugs na landing

--- script/script.js
+++ script/script.js
@@ -3,9 +3,8 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
+        } 
+        
     });
 }, {
     threshold: 0.51,
